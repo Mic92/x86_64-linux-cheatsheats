14d608c0cc1536a8d6bf89770f3eca3c94bdacb9
Jörg Thalheim <joerg@thalheim.io>
2018-03-20 14:43:28 +0000

    extend errnos

--- errno.py
+++ errno.py
@@ -5,7 +5,15 @@ import os
 def main():
     with open("pages/errno", "w") as f:
         for k, v in sorted(os.errno.errorcode.items()):
-            f.write("{:>3} {:>15} {}\n".format(k, v, os.strerror(k)))
+            name = os.strerror(k)
+            content = "{:>3} {:>15} {}\n".format(k, v, name)
+            f.write(content)
+            try:
+                with open(f"pages/{v}", "w") as f2:
+                    f2.write(content)
+                    os.symlink(f"{v}", f"pages/{v.lower()}")
+            except FileExistsError:
+                pass
 
 
 if __name__ == '__main__':
