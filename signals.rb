468c53bbe6a2b368b274e3701ff53a2c685368d3
Jörg Thalheim <joerg@thalheim.io>
2019-10-30 13:29:41 +0000

    fix signals.rb

--- signals.rb
+++ signals.rb
@@ -9,26 +9,28 @@ header = "Signal    Value  Action Comment"
 signals_file = open("pages/signals", "w")
 signal_file = nil
 f.each do |line|
-  if line.match(/Signal\tValue\tAction\tComment/)
+  if line.match(/Signal	Standard	Action	Comment/)
     found = true
     next
   end
-  next unless found
-  if line.match(/^\.TE/)
-    found = false
-    next
-  end
-  line.gsub!(/\\fB|\\P|\\fP|\\/, "")
-  name, num, action, description = line.split("\t")
-  signals_file.puts("#{name.ljust(9)} #{num.ljust(8)} #{action.ljust(4)} #{description}")
-  if name != ""
-    filename = "pages/#{name}"
-    signal_file = open(filename, "w")
-    signal_file.puts("Value  Action Comment")
-    FileUtils.ln_sf filename, filename.downcase
-  end
-  unless signal_file.nil?
-    signal_file.puts("#{num.ljust(8)} #{action.ljust(4)} #{description}")
+  if found
+    if line.match(/^\.TE/)
+      found = false
+      next
+    end
+    line.gsub!(/\\fB|\\P|\\fP|\\/, "")
+    name, _, action, description = line.split("\t")
+    num = Signal.list[name.gsub("SIG", "")].to_s
+    signals_file.puts("#{name.ljust(9)} #{num.ljust(8)} #{action.ljust(4)} #{description}")
+    if name != ""
+      filename = "pages/#{name}"
+      signal_file = open(filename, "w")
+      signal_file.puts("Value  Action Comment")
+      FileUtils.ln_sf File.basename(filename), filename.downcase
+    end
+    unless signal_file.nil?
+      signal_file.puts("#{num.ljust(8)} #{action.ljust(4)} #{description}")
+    end
   end
 end
 
